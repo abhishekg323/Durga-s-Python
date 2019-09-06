34fc0b406b4f7fb5e36a2e5cb2fcf47fc6bc53b5
renuka <asodaria.renuka19@gmail.com>
2019-09-06 15:30:07 -0400

    this is all about relational operator

--- python_operators.py
+++ python_operators.py
@@ -50,5 +50,85 @@ print('Meera' * int('3'))
 print('Hiren' * True)
 print('Anu'* False)
 
+#Relational operators:
+a=10
+b=20
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+s1 = 'a'
+s2 = 'b'
+print(ord(s1))
+print(ord(s2))
+print(chr(97))
+print(ord('A'))
+
+s1 = 'Shiva'
+s2 = 'Nena'
+print(s1>s2)
+print(s1<s2)
+print(s1>=s2)
+print(s1<=s2)
+
+s1 = 'Diza'
+s2 = 'Diza'
+print(s1>s2)
+print(s1<s2)
+print(s1>=s2)
+print(s1<=s2)
+
+s1 = 'Shiva'
+s2 = 'shiva'
+print(s1>s2)
+print(s1<s2)
+print(s1>=s2)
+print(s1<=s2)
+
+print(True > False)
+print(True < False)
+print(True >= False)
+print(True <= False)
+
+a= 10
+b = 20
+if a >b:
+    print('a is greater than b')
+else:
+    print('a is less than b')
+
+print(10<20)
+print(10<20<30)
+print(10<20<30<40)
+print(10<20<30>40>50)  #if any one statement will be false than result will be false
+
+#equality operators:
+print(10==20)
+print(10!=20)
+print(1==True)
+print(10==10.0)
+print('Shiva' == 'Shiva')
+print(10 == 'Hiren')
+print(10=='10')
+print(10==20==30==40)
+print(10==10==10==10)
+
+# difference between == operator and 'is' operator
+l1 = [10,20,30,40]
+l2 = [10,20,30,40]
+print(id(l1))
+print(id(l2))
+
+print(l1==l1)
+print(l1 is l2)
+
+l3 = l1
+print(l1 is l3)
+print(l1==l3)
+
+#logical operators for boolean type:
+
+
 
 
