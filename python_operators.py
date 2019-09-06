6807ba7918bd8771e38a58f318d95f61fb155511
renuka <asodaria.renuka19@gmail.com>
2019-09-06 15:03:33 -0400

    this is all about arithmetic operator

---
+++ python_operators.py
@@ -0,0 +1,54 @@
+#Arithmetica operator
+'''
+/ = normal division
+// = floor division
+
+'''
+a=10
+b= 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b) # it will give reminder ans
+print(10/2) #normal division operator
+print(a/b)
+print(10/3) # floor division operator and its meant for integral and floating value
+print(10//2)
+print(10//3)
+print(10.0/2)
+print(10/3)
+print(10.0/3)
+print(10//3)
+print(10.0//3)
+
+print(20/2)
+print(20.5/2)
+print(20//2)
+print(20.0/2)
+print(30//2)
+
+print(10**2) # here ** is exponetial
+print(3**3)
+
+print('Shiva'+'Patoliya')
+print('Shiva'+ ' ' + 'Patoliya')
+print('Hiren' + '10')
+print('Renuka' + str(10))
+
+print('Renuka'*3)
+print(3*'Hiren')
+#print('Renuka' * 'Jaya')  we will get error
+# because one argument must be string and one int
+ # print('Jaya' * '3')
+
+print('Meera' * int('3'))
+
+# print(10/0) we will get modulo by zero error
+# print(x/0)
+#print(x.0/0)
+
+print('Hiren' * True)
+print('Anu'* False)
+
+
+
