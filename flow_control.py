e374382c07bcda0562e55c0dde36eb076ef87cc1
renuka <asodaria.renuka19@gmail.com>
2019-09-08 21:53:36 -0400

    This all about input and output statements

---
+++ flow_control.py
@@ -0,0 +1,36 @@
+#Indentation
+# if condtition:
+#     statement 1
+#     statement 2
+#     statement 3
+#     staement 4
+#if statement:
+if 10<20:
+    print('10 is less then 20')
+print('end the program')
+
+name = input('enter name')
+if name == 'Renuka':
+    print('Hello Renuka, good Evening ')
+print('How are you!')
+
+#if-else statement:
+name = input('enter your name here:')
+if name == 'Shiva':
+    print("hello shiva how are you!")
+else:
+    print("hello Guest how are you?")
+print("hello good morning!")
+
+#if-elif-else statement:
+brand = input('enter your fav brand:')
+if brand == 'kf':
+    print('this brand is for kids')
+elif brand == 'ko':
+    print('this is too light brand')
+elif brand == 'fo':
+    print('buy one get one free')
+else:
+    print('other brand is not recommended')
+
+#write programe to find biggest of 2 given no
