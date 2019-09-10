b68f58b3bc1c9e5b0ced9e692eabeafd1657750a
renuka <asodaria.renuka19@gmail.com>
2019-09-10 12:24:57 -0400

    flow control

--- python_flowcontrol-1.py
+++ python_flowcontrol-1.py
@@ -46,5 +46,74 @@ for item in cart:
     print('processing item',item)
 
 l=[10,20,0,40,30,50]
+for x in l:
+    print('100/{}={}'.format(x,100/x))
 
+l=[10,20,0,40,30,50]
+for x in l:
+    if x==0:
+        print('how it it possible divided by zero')
+        continue
+    print('100/{}={}'.format(x,100/x))
 
+#here, we will get error because without for or while loop we can not use continue or break statements
+# x=10
+# if x>40:
+#     print('hello')
+#     continue
+# print('hi')
+
+#break and continue in loop
+for i in range(3):
+    for j in range(3):
+        if i==j:
+            break
+        print(i,j)
+
+for i in range(3):
+    for j in range(3):
+        if i==j:
+            continue
+        print(i,j)
+
+#loops with else block:
+cart = [10,20,30,40,50]
+for item in cart:
+    if item>500:
+        print('we can not process this item because it required insurance')
+        break
+    print('processing iteam in progress',item)
+else:
+    print('congratualtions your all item succeessfully process')
+
+#break
+cart = [10,20,30,40,50,600]
+for item in cart:
+    if item>500:
+        print('we can not process this item because it required insurance')
+        break
+    print('processing iteam in progress',item)
+else:
+    print('congratualtions your all item succeessfully process')
+
+#break
+cart = [10,20,30,40,50,600,50,60]
+for item in cart:
+    if item>500:
+        print('we can not process this item because it required insurance')
+        break
+    print('processing iteam in progress',item)
+else:
+    print('congratualtions your all item succeessfully process')
+
+#continue
+cart = [10,20,30,40,50,600,50,60]
+for item in cart:
+    if item>500:
+        print('we can not process this item because it required insurance')
+        continue
+    print('processing iteam in progress',item)
+else:
+    print('congratualtions your all item succeessfully process')
+
+#pass statement:
