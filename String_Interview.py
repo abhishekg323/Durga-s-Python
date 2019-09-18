83b8a9449275b9a4da1b039091fa02eaab3702da
renuka <asodaria.renuka19@gmail.com>
2019-09-17 22:16:45 -0400

    This is all about string

--- String_Interview.py
+++ String_Interview.py
@@ -59,4 +59,24 @@ while i<len(l):
         l1.append(l[i])
     else:
         l1.append(l[i][::-1])
-print(l1)
+    i=i+1
+    output=' '.join(l1)
+print(output)
+
+#
+s=input('please enter here any string')
+i=0
+print('character present at even index')
+while i<len(s):
+    print(i)
+    i=i+2
+print('character present at odd index')
+i=1
+while i<len(s):
+    print(i)
+    i=i+2
+
+#different ways
+s=input('enter here any no')
+print('print here even no:',s[0::2])
+print('print here odd no:',s[1::2])
